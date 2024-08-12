8d44306812b172b8f62f3bccb089481a44ce9968
Monirul Jim <monirulislamjim6640@gmail.com>
2024-08-12 12:52:31 +0600

    add field and post author and profile

--- author/views.py
+++ author/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . import forms
 # Create your views here.
 
 
-def author(request):
-    return render(request, 'author/author.html')
+def add_author(request):
+    if request.method == "POST":
+        author_form = forms.AuthorForm(request.POST)
+        if author_form.is_valid():
+            author_form.save()
+            return redirect('add-author')
+    else:
+        author_form = forms.AuthorForm()
+    return render(request, 'author/author.html', {'forms': author_form})
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def add_post(request):
+    return render(request, 'posts/post.html')
