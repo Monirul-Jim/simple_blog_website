a2e573c1e4831d90ae5287c5618d1850369281c5
Monirul Jim <monirulislamjim6640@gmail.com>
2024-08-12 21:18:22 +0600

    show blog in front end

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . import forms
+from . models import Post
 # Create your views here.
 
 
@@ -13,3 +14,21 @@ def add_post(request):
     else:
         post = forms.PostForm()
     return render(request, 'posts/post.html', {'post': post})
+
+
+def edit_post(request, id):
+    post_forms = Post.objects.get(pk=id)
+    post = forms.PostForm(instance=post_forms)
+    if request.method == 'POST':
+        post = forms.PostForm(request.POST, instance=post_forms)
+        if post.is_valid():
+            post.save()
+            return redirect('home')
+
+    return render(request, 'posts/post.html', {'post': post})
+
+
+def delete_post(request, id):
+    post_forms = Post.objects.get(pk=id)
+    post_forms.delete()
+    return redirect('home')
--- simple_blog_website/views.py
+++ simple_blog_website/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from posts.models import Post
 
 
 def home(request):
-    return render(request, 'home.html')
+    data = Post.objects.all()
+    # for i in data:
+    #     for j in i.category.all():
+    #         print(j)
+    # print(data)
+    return render(request, 'home.html', {'data': data})
