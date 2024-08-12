ecbe2ad76e8fb4268ea2d65d11705ecaef96b3a0
Monirul Jim <monirulislamjim6640@gmail.com>
2024-08-12 18:22:38 +0600

    add post

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . import forms
 # Create your views here.
 
 
 def add_category(request):
-    return render(request, 'categories/category.html')
+    if request.method == 'POST':
+        category = forms.CategoryForm(request.POST)
+        if category.is_valid():
+            category.save()
+            return redirect('add_category')
+    else:
+        category = forms.CategoryForm()
+    return render(request, 'categories/category.html', {'category': category})
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . import forms
 # Create your views here.
 
 
 def add_post(request):
-    return render(request, 'posts/post.html')
+    if request.method == 'POST':
+        post = forms.PostForm(request.POST)
+        if post.is_valid():
+            post.save()
+            return redirect('add_post')
+    else:
+        post = forms.PostForm()
+    return render(request, 'posts/post.html', {'post': post})
