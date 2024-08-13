99c66f41a46e4315a59b283c413d532706a00d20
Monirul Jim <monirulislamjim6640@gmail.com>
2024-08-13 18:11:50 +0600

    user signup

--- signup/views.py
+++ signup/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
+from django.contrib import messages
 # Create your views here.
 
 
 def signup(request):
-    return render(request, 'signup/signup.html')
+    if request.method == 'POST':
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "User Created Successfully")
+            form.save()
+        else:
+            print(form.errors)
+    else:
+        form = forms.RegisterForm()
+    return render(request, 'signup/signup.html', {'forms': form})
 
 
 def login(request):
